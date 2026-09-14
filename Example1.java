18f3cb4b406786247e835d499d8da6cde483dfc5
272145 <2765929@learn.vsb.bc.ca>
2026-09-14 15:01:12 -0700

    Add files via upload

--- Example1.java
+++ Example1.java
@@ -17,7 +17,16 @@ public class Example1{
 		int year = 2026;
 		System.out.println("Today is " + " "+ year + " " + month + " " + day);
 		//Ex 2.3
-		
+		int hour = 14;
+		int minute = 50;
+		int second;
+		second = minute *60 + hour * 3600;
+		System.out.println("Current time is:");
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.print(minute);
+		System.out.println("");
+		System.out.println(" It has been" + " " + second + " " + "Seconds since midnight");
 	
 
 	}
