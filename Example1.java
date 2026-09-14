621f6b93c92983c7966752917fa909d2932bf37a
272145 <2765929@learn.vsb.bc.ca>
2026-09-14 14:50:11 -0700

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,25 @@
+
+public class Example1{
+	public static void main(String[] args){
+		String name;
+		int age;
+		name = "George";
+		age = 16;
+		System.out.println("My name is" + " " + name + " " + "and I am" + " " + age);
+		System.out.println(7/2);
+		System.out.println(7.0/2);
+		System.out.println(7%2);
+		
+		// Ex 2.2
+		int day = 14;
+		String month;
+		month  = "September";
+		int year = 2026;
+		System.out.println("Today is " + " "+ year + " " + month + " " + day);
+		//Ex 2.3
+		
+	
+
+	}
+}
+
